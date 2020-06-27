c08845bd0905aa880ded6593f55d155d92ffb0e4
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-06-26 22:31:04 -0300

    add JWT authentication

--- api/server/routes/UserRoutes.js
+++ api/server/routes/UserRoutes.js
@@ -3,9 +3,11 @@ import UserController from '../controllers/UserController';
 
 const router = Router();
 
-router.get('/', UserController.getAllUsers);
-router.post('/', UserController.addUser);
+router.post('/login', UserController.signInUser);
+
+router.get('/', UserController.verifyToken, UserController.getAllUsers);
 router.get('/:id', UserController.getUser);
+router.post('/', UserController.addUser);
 router.put('/:id', UserController.updatedUser);
 router.delete('/:id', UserController.deleteUser);
 
