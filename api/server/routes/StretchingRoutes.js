ed7b5d09460f832ac26a4db9595edf748ee79342
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-06-30 10:32:06 -0300

    add stretching routes

---
+++ api/server/routes/StretchingRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import StretchingController from '../controllers/StretchingController';
+
+const router = Router();
+
+
+router.get('/', StretchingController.getAllStretchings);
+router.get('/:id', StretchingController.getStretching);
+router.post('/', StretchingController.addStretching);
+router.put('/:id', StretchingController.updatedStretching);
+router.delete('/:id', StretchingController.deleteStretching);
+
+export default router;
