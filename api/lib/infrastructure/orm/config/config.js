0d7bef6ad502a100eed46c6aa021ea532a4eb726
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-07-31 20:41:29 -0300

    create DATABASE_URL for production env

--- api/lib/infrastructure/orm/config/config.js
+++ api/lib/infrastructure/orm/config/config.js
@@ -18,9 +18,5 @@ export const test = {
 }
 
 export const production = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
+  use_env_variable: process.env.DATABASE_URL,
 }
